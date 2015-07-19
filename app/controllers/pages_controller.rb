d82f8a0e78ac26a51980bca11a5e504b765e237c
keyannam <keyannamayfield@gmail.com>
2015-07-19 15:28:20 -0500

    Created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def home
     if current_user
       redirect_to wines_path
     end
+  end
+
+  def dashboard
+    @wines = current_user.wines
   end
 end
